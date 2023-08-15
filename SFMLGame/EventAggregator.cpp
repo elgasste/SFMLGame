61507d6cc9e87fba237cdf7330c7325006212b01
selgas <s.elgas@techsmith.com>
2023-08-15 19:36:43 -0400

    Missed a reference on the last commit

--- SFMLGame/EventAggregator.cpp
+++ SFMLGame/EventAggregator.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void EventAggregator::AddListener( GameEvent event, IEventListener* listener )
 {
-   auto listeners = _listenersMap[event];
+   auto& listeners = _listenersMap[event];
 
    if ( find( listeners.begin(), listeners.end(), listener ) == listeners.end() )
    {
