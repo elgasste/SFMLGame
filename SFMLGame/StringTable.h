fc79f1334f5baa489bb6942fbcb7915fd5469d76
selgas <s.elgas@techsmith.com>
2023-08-23 18:03:20 -0400

    More descriptive playing state message

--- SFMLGame/StringTable.h
+++ SFMLGame/StringTable.h
@@ -18,11 +18,8 @@
 #define IDS_BallAngle               "Ball Angle:         {}"
 #define IDS_BallVelocity            "Ball Velocity:      {}"
 
-// title screen state
-#define IDS_TitleStateMessage       "Press any key"
-
 // playing state
-#define IDS_PlayStateMessage        "Press ESC for menu, or F12 to toggle diagnostics"
+#define IDS_PlayStateMessage        "Press ESC for menu, F12 for diagnostics, or maybe try the arrow keys?"
 
 // menu
 #define IDS_MenuOptionStartGame     "Start Game"
